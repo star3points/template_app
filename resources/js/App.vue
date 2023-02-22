78cbea1a0b6e775c8a2358e25eb07003b9f90b00
Aleksczech <dan235497@gmail.com>
2023-02-22 10:33:18 +0300

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Laravel 10 <br>
+        Vue 3 <br>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    created() {
+        console.log('App.vue created')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import {createApp} from 'vue'
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import App from './App.vue'
+
+createApp(App).mount("#app")
